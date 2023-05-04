132b7ff274ecb043113980ad7572af506aec5f9b
EduardoPereira22 <107266971+EduardoPereira22@users.noreply.github.com>
2023-05-04 07:23:45 -0300

    Delete appexercicios.py

--- appexercicios.py
+++
@@ -1,7 +0,0 @@
-##Tirando de 2 numeros flutuantes no python, simulando uma nota de escola
-x = float(input('Digite Sua primeira nota '))
-y = float(input('Digite sua segunda nota '))
-media = ('O seu Resultado final foi {}') .format ((x+y)/2)
-print(media)
-
-
